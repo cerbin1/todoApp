0da143c05532e822b8ed0a34af2c65a354a6f497
Bartek <gekarez@gmail.com>
2017-12-12 20:46:06 +0100

    Moved buttons to click list item to right, and item name to left

--- app/src/main/java/com/example/bartek/todoapp/TodoListActivity.java
+++ app/src/main/java/com/example/bartek/todoapp/TodoListActivity.java
@@ -3,7 +3,9 @@ package com.example.bartek.todoapp;
 import android.database.Cursor;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -26,10 +28,11 @@ public class TodoListActivity extends AppCompatActivity {
     private void displayListElements() {
         String nameOfList = getIntent().getStringExtra("nameOfList");
         Cursor data = database.getItems(nameOfList);
-        LinearLayout linearLayout = findViewById(R.id.linearLayout);
+        LinearLayout container = findViewById(R.id.linearLayout);
         while (data.moveToNext()) {
             final TextView itemName = new TextView(this);
             itemName.setText(data.getString(0));
+            itemName.setTextSize(20);
             Button button = new Button(this);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -38,8 +41,18 @@ public class TodoListActivity extends AppCompatActivity {
                 }
             });
             button.setText("Done");
-            linearLayout.addView(button);
-            linearLayout.addView(itemName);
+            LinearLayout newEntryLayout = new LinearLayout(this);
+            newEntryLayout.addView(itemName, getLayoutParamsForItem());
+            newEntryLayout.addView(button);
+            container.addView(newEntryLayout);
         }
+    }
+
+    @NonNull
+    private LinearLayout.LayoutParams getLayoutParamsForItem() {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.weight = 1.0f;
+        params.gravity = Gravity.START;
+        return params;
     }
 }
